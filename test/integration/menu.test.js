67de962adb8f99afee9a68eaa1dc4bb1a18c7623
shareni <luka.markovic.it@protonmail.com>
2022-01-22 00:08:14 +0100

    reversed properties to property
    .to.have.properties() to .to.have.property()

--- test/integration/menu.test.js
+++ test/integration/menu.test.js
@@ -20,19 +20,19 @@ describe('Menu Integration Test', function () {
 
 		const response = await request(app).post('/v1/menu').send(data);
 
-		expect(response).to.have.properties('header', 'status', 'body');
+		expect(response).to.have.property('header');
+		expect(response).to.have.property('status');
+		expect(response).to.have.property('body');
 
 		expect(response.headers['content-type']).to.match(/^application\/json/);
 		expect(response.status).to.equal(200);
 
-		expect(response.body).to.have.properties(
-			'user',
-			'language',
-			'name',
-			'items',
-			'showIngredients',
-			'showAmounts'
-		);
+		expect(response.body).to.have.property('user');
+		expect(response.body).to.have.property('language');
+		expect(response.body).to.have.property('name');
+		expect(response.body).to.have.property('items');
+		expect(response.body).to.have.property('showIngredients');
+		expect(response.body).to.have.property('showAmounts');
 
 		menuCache = response.body;
 		expect(response.body.user).to.equal(data.user.toString());
@@ -44,10 +44,15 @@ describe('Menu Integration Test', function () {
 	});
 
 	it('should update a menu', async function () {
-		const data = { name: 'updated name' };
+		const data = {
+			name: 'updated name',
+		};
+
 		const response = await request(app).put(`/v1/menu/${menuCache._id}`).send(data);
 
-		expect(response).to.have.properties('header', 'status', 'body');
+		expect(response).to.have.property('header');
+		expect(response).to.have.property('status');
+		expect(response).to.have.property('body');
 
 		expect(response.body.user).to.equal(menuCache.user.toString());
 		expect(response.body.language).to.equal(menuCache.language);
@@ -60,19 +65,19 @@ describe('Menu Integration Test', function () {
 	it('should retrieve a menu', async function () {
 		const response = await request(app).get(`/v1/menu/${menuCache._id}`);
 
-		expect(response).to.have.properties('header', 'status', 'body');
+		expect(response).to.have.property('header');
+		expect(response).to.have.property('status');
+		expect(response).to.have.property('body');
 
 		expect(response.headers['content-type']).to.match(/^application\/json/);
 		expect(response.status).to.equal(200);
 
-		expect(response.body).to.have.properties(
-			'user',
-			'language',
-			'name',
-			'items',
-			'showIngredients',
-			'showAmounts'
-		);
+		expect(response.body).to.have.property('user');
+		expect(response.body).to.have.property('language');
+		expect(response.body).to.have.property('name');
+		expect(response.body).to.have.property('items');
+		expect(response.body).to.have.property('showIngredients');
+		expect(response.body).to.have.property('showAmounts');
 
 		menuCache = response.body;
 		expect(response.body.user).to.equal(menuCache.user.toString());
@@ -86,7 +91,9 @@ describe('Menu Integration Test', function () {
 	it('should retrieve all menus for a user', async function () {
 		const response = await request(app).get('/v1/menus').query({ user: menuCache.user });
 
-		expect(response).to.have.properties('header', 'status', 'body');
+		expect(response).to.have.property('header');
+		expect(response).to.have.property('status');
+		expect(response).to.have.property('body');
 
 		expect(response.body).to.be.an.instanceOf(Array);
 		expect(response.body).to.have.lengthOf(1);
@@ -102,7 +109,9 @@ describe('Menu Integration Test', function () {
 	it('should delete a menu', async function () {
 		const response = await request(app).delete(`/v1/menu/${menuCache._id}`);
 
-		expect(response).to.have.properties('header', 'status', 'body');
+		expect(response).to.have.property('header');
+		expect(response).to.have.property('status');
+		expect(response).to.have.property('body');
 
 		expect(response.body).to.eql({ deletedCount: 1 });
 	});
