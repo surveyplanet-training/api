769002d5f0f3e0a190381ad277c5bd91b98ba368
Luka Markovic <luka.markovic.it@protonmail.com>
2022-03-24 20:52:00 +0100

    tried to set up /metrics

---
+++ routes/v1/metrics.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const client = require('prom-client');
+
+// Create a Registry which registers the metrics
+const register = new client.Registry();
+
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+	app: 'menu-api',
+});
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
+
+module.exports = function (router) {
+	router.get('/metrics', (request, response, next) => {
+		response.setHeader('Content-Type', register.contentType);
+		response.end(register.metrics());
+	});
+
+	return router;
+};
