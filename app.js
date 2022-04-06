c1027bddd72ea06f168b9d322dc06252ca49eea5
Luka Markovic <luka.markovic.it@protonmail.com>
2022-04-06 12:57:23 +0200

    testing request duration histogram

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const config = require('./lib/config');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const { measureRequestDuration } = require('./lib/monitor');
-const {skip, format} = require('./lib/logger');
-
+const { measureRequestDuration } = require('./lib/monitor');
+const { skip, format } = require('./lib/logger');
 
 // const loggerMiddleware = connectLogger(logger);
 
@@ -13,23 +12,22 @@ const {skip, format} = require('./lib/logger');
 const routes = require('./routes/index');
 const metrics = require('./routes/metrics');
 
-mongoose.connect(config.mongo, config.mongoOptions)
+mongoose
+	.connect(config.mongo, config.mongoOptions)
 	.then(() => console.log('Connected to MongoDB'))
 	.catch(console.error);
 
 const app = express();
 
 app.set('port', config.port); // use: app.get('port')
-app.use( bodyParser.json() );
-app.use( morgan( format, {skip:skip} ) );
-// app.use( measureRequestDuration );
-
+app.use(bodyParser.json());
+app.use(morgan(format, { skip: skip }));
+app.use(measureRequestDuration);
 
 // all urls will be accessed through /v1/... ( e.g. /v1/users, /v2/foods, etc.)
 // This allows us to have a versioned API so we can make changes to routes
 // if we need to while still keeping old routes aournd for legacy clients
 app.use('/v1', routes());
-app.use('/', metrics() );
-
+app.use('/', metrics());
 
 module.exports = app;
