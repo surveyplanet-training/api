bb204ce224ade18a94ae10be928ee763c973410a
Luka Markovic <luka.markovic.it@protonmail.com>
2022-05-16 20:47:52 +0200

    commenting out monitoring,logging, and probes for a test

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./lib/config');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { measureRequestDuration } = require('./lib/monitor');
-const { skip, format } = require('./lib/logger');
+// const morgan = require('morgan');
+// const { measureRequestDuration } = require('./lib/monitor');
+// const { skip, format } = require('./lib/logger');
 
 // const loggerMiddleware = connectLogger(logger);
 
 // App routes
-const routes = require('./routes/index');
-const metrics = require('./routes/metrics');
+// const routes = require('./routes/index');
+// const metrics = require('./routes/metrics');
 
 mongoose
 	.connect(config.mongo, config.mongoOptions)
@@ -21,13 +21,13 @@ const app = express();
 
 app.set('port', config.port); // use: app.get('port')
 app.use(bodyParser.json());
-app.use(morgan(format, { skip: skip }));
-app.use(measureRequestDuration);
+// app.use(morgan(format, { skip: skip }));
+// app.use(measureRequestDuration);
 
-// all urls will be accessed through /v1/... ( e.g. /v1/users, /v2/foods, etc.)
-// This allows us to have a versioned API so we can make changes to routes
-// if we need to while still keeping old routes aournd for legacy clients
-app.use('/v1', routes());
-app.use('/', metrics());
+// // all urls will be accessed through /v1/... ( e.g. /v1/users, /v2/foods, etc.)
+// // This allows us to have a versioned API so we can make changes to routes
+// // if we need to while still keeping old routes aournd for legacy clients
+// app.use('/v1', routes());
+// app.use('/', metrics());
 
 module.exports = app;
